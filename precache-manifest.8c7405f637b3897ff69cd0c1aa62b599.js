1505c3d8706b78115b4a3bc18356ceee6cac1f91
Vladislav Shulepov <shulepov.vlad@gmail.com>
2020-05-23 13:01:39 +0300

    Updates

---
+++ precache-manifest.8c7405f637b3897ff69cd0c1aa62b599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daff5d95c2973ff2f2f1b37633fae3ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a42767e3417886c3e8a2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a48f67a7bf91c6a5f34d",
+    "url": "/static/js/2.a4e06fa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4e06fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42767e3417886c3e8a2",
+    "url": "/static/js/main.a238de04.chunk.js"
+  },
+  {
+    "revision": "b6ebbf9d3a57d0f1ef9d",
+    "url": "/static/js/runtime-main.5eeb4e9f.js"
+  }
+]);
